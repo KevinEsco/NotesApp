7b302f12ac547babf128b40dd8f238c39f985812
KevinEscobedo <kevinescobedo248@gmail.com>
2022-03-01 01:57:46 -0300

    Aggregate Note repository with implementation, and Theme name changed

--- app/src/main/java/com/kevinesco/notesapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/kevinesco/notesapp/feature_note/presentation/MainActivity.kt
@@ -3,13 +3,13 @@ package com.kevinesco.notesapp.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.kevinesco.notesapp.ui.theme.CleanArchitectureNoteAppTheme
+import com.kevinesco.notesapp.ui.theme.NotesAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CleanArchitectureNoteAppTheme {
+            NotesAppTheme {
                 
             }
         }
